88fcdf2af9b846af2371172c724d89bd6be188d8
kajan-m <72370580+kajan-m@users.noreply.github.com>
2025-05-06 18:48:15 +1000

    Update main.go

--- apps/customers/main.go
+++ apps/customers/main.go
@@ -60,7 +60,6 @@ func main() {
 		newrelic.ConfigLicense(os.Getenv("NEW_RELIC_LICENSE_KEY")),
 		newrelic.ConfigDebugLogger(os.Stdout),
 		newrelic.ConfigDistributedTracerEnabled(true),
-		newrelic.ConfigAppLogForwardingEnabled(true),
 	)
 	if nil != err {
 		log.Print(err)
